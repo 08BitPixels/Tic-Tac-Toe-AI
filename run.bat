362f641574600db217552ff5d46395b596d8cb48
Rohan Garner <161069703+08BitPixels@users.noreply.github.com>
2024-07-30 16:25:49 +0100

    Update run.bat
    - To accommodate moving of python

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @echo off
-"C:\Users\rohan\AppData\Local\Microsoft\WindowsApps\python3.12.exe" "C:\Users\rohan\OneDrive\Documents\Coding Projects\Python\Pygame\Tic Tac Toe AI\main.py"
+"D:\rohan\AppData\Python3.12\python.exe" "D:\rohan\OneDrive\Documents\Coding Projects\Python\Pygame\Tic Tac Toe AI\main.py"
 pause
